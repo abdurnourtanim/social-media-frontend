e929abb3615708a0d7f091e4fbf41f2270d77cdc
antanim <abdurnouutanim11@gmail.com>
2022-04-21 20:53:59 +0600

    like functionality working fine

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,19 @@
 import { MoreVert } from "@material-ui/icons";
-import React from "react";
+import React, { useState } from "react";
 import img4 from "../assets/heart.png";
 import img3 from "../assets/like.png";
 import { Users } from "../fakeData";
 import "../styles/post.css";
 
 const Post = ({ post }) => {
+  const [like, setLike] = useState(post.like);
+  const [isLiked, setIsLiked] = useState(false);
+
+  const likeHandler = () => {
+    setLike(isLiked ? like - 1 : like + 1);
+    setIsLiked(!isLiked);
+  };
+
   return (
     <div className="post">
       <div className="postWrapper">
@@ -34,9 +42,19 @@ const Post = ({ post }) => {
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
-            <img className="likeIcon" src={img3} alt="" />
-            <img className="likeIcon" src={img4} alt="" />
-            <span className="postLikeCounter">{post.like} people like it</span>
+            <img
+              className="likeIcon"
+              src={img3}
+              onClick={likeHandler}
+              alt="like"
+            />
+            <img
+              className="likeIcon"
+              src={img4}
+              onClick={likeHandler}
+              alt="love"
+            />
+            <span className="postLikeCounter">{like} people like it</span>
           </div>
           <div className="postBottomRight">
             <span className="postCommentText">{post.comment} comments</span>
